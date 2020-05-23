fb590ac0dee9ecdc866fb64ff8b118bc032325f0
Dhanesh <dhaneshspund@gmail.com>
2020-05-24 00:17:56 +0530

    DI-4 : i] ORM for table Address        ii] Implementation of foreign key constraint for table Address        iii] Seperated base class to hold all the tables

---
+++ model/Address.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Column, String, Table, Integer, Date, ForeignKey, create_engine
+from model.ORMbase import Base
+from sqlalchemy.orm import relationship
+from Database_connection.DBconnection import DBconnection
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    aid = Column(Integer, primary_key=True)
+    cid = Column(Integer, ForeignKey("customer.cid"))
+    description = Column(String)
+    state = Column(String)
+    district = Column(String)
+    pin_no = Column(String)
+    add=relationship("Customer",back_populates="addresses")
+
+    def add_address(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        session.add(self)
+        session.commit()
+        session.close()
+
+    def get_address(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customer_id = self.cid
+        result = session.query(Address).filter(Address.cid == customer_id)
+        session.close()
+        return result
+
+    def update_address(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        addressId = self.aid
+        existingAddress = session.query(Address).get(addressId)
+        existingAddress.cid = self.cid
+        existingAddress.description = self.description
+        existingAddress.state = self.state
+        existingAddress.district = self.district
+        existingAddress.pin_no = self.pin_no
+        session.commit()
+        session.close()
+
+    def delete_address(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customer_id = self.cid
+        session.query(Address).filter(Address.cid == customer_id).delete()
+        session.commit()
+        session.close()
---
+++ model/Customer.py
@@ -0,0 +1,66 @@
+from sqlalchemy import Column, String, Table, Integer, Date
+from model.Address import Address
+from model.ORMbase import Base
+from sqlalchemy.orm import relationship
+from Database_connection.DBconnection import DBconnection
+
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    cid = Column(Integer, primary_key=True)
+    firstname = Column(String)
+    surname = Column(String)
+    email = Column(String)
+    password = Column(String)
+    dob = Column(Date)
+    mobile_number = Column(String)
+    addresses=relationship("Address",back_populates='add')
+
+    def get_customer(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customer_id = self.cid
+        fetchedCustomer = session.query(Customer).get(customer_id)
+        session.close()
+        return fetchedCustomer
+
+    def add_customer(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        session.add(self)
+        session.commit()
+        session.close()
+
+    def get_all_customers(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customer_list = session.query(Customer).all()
+        session.close()
+        return customer_list
+
+    def update_customer(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customerId = self.cid
+        existingCustomer = session.query(Customer).get(customerId)
+        existingCustomer.firstname = self.firstname
+        existingCustomer.surname = self.surname
+        existingCustomer.email = self.email
+        existingCustomer.password = self.password
+        existingCustomer.dob = self.dob
+        existingCustomer.mobile_number = self.mobile_number
+        session.commit()
+        session.close()
+
+    def delete_customer(self):
+        customerId = self.cid
+        address=Address(cid=customerId)
+        address.delete_address()
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        existingCustomer = session.query(Customer).get(customerId)
+        session.delete(existingCustomer)
+        session.commit()
+        session.close()
+
+
