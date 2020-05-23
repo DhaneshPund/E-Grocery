c72b244c927f1a7511aaea74674deb91b2b71ebc
Dhanesh <dhaneshspund@gmail.com>
2020-05-23 18:06:04 +0530

    DI-3 : CRUD operations for table Customer using ORM

--- Model/Customer.py
+++ Model/Customer.py
@@ -14,3 +14,37 @@ class Customer(Base):
     password = Column(String)
     dob = Column(Date)
     mobile_number = Column(String)
+
+    def add_customer(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        session.add(self)
+        session.commit()
+
+    def get_all_customers(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customer_list = session.query(Customer).all()
+        return customer_list
+
+    def update_customer(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customerId = self.cid
+        exstingCustomer = session.query(Customer).get(customerId)
+        print(exstingCustomer, "cid: ", customerId)
+        exstingCustomer.firstname = self.firstname
+        exstingCustomer.surname = self.surname
+        exstingCustomer.email = self.email
+        exstingCustomer.password = self.password
+        exstingCustomer.dob = self.dob
+        exstingCustomer.mobile_number = self.mobile_number
+        session.commit()
+
+    def delete_customer(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        customerId = self.cid
+        exstingCustomer = session.query(Customer).get(customerId)
+        session.delete(exstingCustomer)
+        session.commit()
