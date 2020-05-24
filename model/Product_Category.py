6d9ed93f98d613ec89a089ca01ccdfa7b51a108e
Dhanesh <dhaneshspund@gmail.com>
2020-05-24 11:36:40 +0530

    DI-7 : i] ORM for table 'product_category'        ii] Crud operations for table 'product_category'

---
+++ model/Product_Category.py
@@ -0,0 +1,50 @@
+from sqlalchemy import Column, Integer, String
+from Database_connection.DBconnection import DBconnection
+
+from model.ORMbase import Base
+
+
+def get_all_product_category():
+    db = DBconnection()
+    session = db.getConnection_parameters()
+    productCategoryList = session.query(ProductCategory).all()
+    session.close()
+    return productCategoryList
+
+
+class ProductCategory(Base):
+    __tablename__ = 'product_category'
+    cat_id = Column(Integer, primary_key=True)
+    catname = Column(String)
+
+    def get_product_category(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        catId = self.cat_id
+        productCategory = session.query(ProductCategory).get(catId)
+        session.close()
+        return productCategory
+
+    def add_product_category(self):
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        session.add(self)
+        session.commit()
+        session.close()
+
+    def delete_product_category(self):
+        categoryId = self.cat_id
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        session.query(ProductCategory).filter(ProductCategory.cat_id == categoryId).delete()
+        session.commit()
+        session.close()
+
+    def update_product_category(self):
+        categoryId = self.cat_id
+        db = DBconnection()
+        session = db.getConnection_parameters()
+        existing_productCategory = session.query(ProductCategory).get(categoryId)
+        existing_productCategory.catname = self.catname
+        session.commit()
+        session.close()
